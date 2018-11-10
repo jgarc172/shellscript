c0a5b392531a43f44b9c90b1e10729db9d50680a
jgarc172 <jgarc172@mail.ccsf.edu>
2018-11-09 18:46:27 -0800

    directories in PATH

--- assign_wk3.sh
+++ assign_wk3.sh
@@ -7,32 +7,25 @@ echo "You are logged in to $HOSTNAME"
 
 # Display the date and time you last logged in, not including this time.
 (last -2 $LOGNAME | grep $LOGNAME | tr -s " " | cut -d" " -f4-7)
-tmp="$(last -2 $LOGNAME | grep $LOGNAME | tr -s " " | cut -d" " -f4-7)"
-echo "$tmp"
-count=$(echo "$tmp" | wc -l)
+sessions="$(last -2 $LOGNAME | grep $LOGNAME | tr -s " " | cut -d" " -f4-7)"
+count=$(echo "$sessions" | wc -l)
+login_date=$(echo "$sessions" | sed -n "${count}p")
+echo "You last logged in on $login_date"
 line1=$(echo "$tmp" | sed -n '1p')
 line2=$(echo "$tmp" | sed -n '2p')
 
-count=$(last -2 $LOGNAME | wc -l )
-if [ $count -eq 2 ] 
-then 
-    line=$(sed )
-else 
-        echo "a is not equal" 
-fi 
+# Displays how many local (non-environment) and environment variables you have defined.
 
-pseudocode:
-    . set count = the number of lines in the command (last -2 $LOGNAME)
-    . if count == 2
-        set line = second line
-      else
-        set line = first line
-    . set login_date = the range of columns 4-7
-    . set msg = "You last logged in on login_date"
-    . print msg
+count_env=$(env | wc -l)
+count_all=$(set | wc -l)
+count_non=$(( $count_all - $count_env ))
+echo "Variables - non-environment: $count_non , environment: $count_env"
 
-- Displays how many local (non-environment) and environment variables you have defined. (This requires arithmetic.)
-- Displays the directories in your search path, in the order they are searched, one-per-line.
+# Displays the directories in your search path, in the order they are searched, one-per-line.
+
+dirs=$(echo $PATH | tr ":" " ")
+for dir in $dirs; do echo $dir; done
+
 2. Takes three valid integers from the user. Make sure they are valid.
 
 3. Finds the largest and smallest number of the three integers.
