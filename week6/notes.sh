a81f0dfb75a1caddbdb542965e9ab537ca087210
jgarc172 <jgarc172@mail.ccsf.edu>
2018-12-08 09:48:30 -0800

    update

--- week6/notes.sh
+++ week6/notes.sh
@@ -6,3 +6,15 @@ done
 for item in item1 item2 item3; do
   echo "$item"
 done
+
+# For generating lists, use wildcards instead of command substitiution.
+# command substitution
+for item in $(ls); do
+  echo "$item"
+done
+
+# better: wildcards
+for item in *; do
+  echo "$item"
+done
+
